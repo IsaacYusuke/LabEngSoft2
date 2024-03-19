a355691c46ea9d8a77c018ab65e8c866ec137a11
lucas-pvg <lucaspvg1@gmail.com>
2024-03-19 09:45:03 -0300

    🚀 N peguntem

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,14 +1,38 @@
 import React from "react";
 import Logo from "../logo/logo";
+import UserPhoto from "../userphoto/userphoto";
+import Button from "../button/button";
+import Tabs from "../tabs/tabs";
 
 import "./navbar.css";
 
-function Navbar(props) {
-    const classes = "navbar " + props.className;
+function Navbar({ className }) {
+    const classes = `navbar ${className}`;
 
     return (
         <div className={classes}>
-            <Logo />
+            <div className="header">
+                <Logo className="black" />
+            </div>
+            
+            <div className="content">
+                <UserPhoto name="Lucas Garieri" />
+                <Tabs 
+                    labels={["Overview", "Pacientes", "Agenda", "Histórico"]}
+                    to={["/overview", "/patients", "", "/appointments"]}
+                />
+            </div>
+            
+
+            <div className="footer">
+                <Button 
+                    className="black small outline"
+                    text="Logout"
+                    icon="left"
+                    iconType="logout"
+                    to=""
+                />
+            </div>
         </div>
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,24 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./pages/Layout/Layout";
-import Layout2 from "./pages/Layout2/Layout2";
+import LayoutPage from './pages/layoutpage/layoutpage';
+import OverviewPage from './pages/professional/overviewpage/overviewpage';
+
+import PatientsPage from './pages/patientspage/patientspage';
+import AppointmentsPage from './pages/appointmentspage/appointmentspage';
+import PaymentsPage from './pages/paymentspage/paymentspage';
+import TestPage from './pages/testpage/testpage';
 import App from './App';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
 
-          <Route index element={<Layout />} />
-          <Route path="teste" element={<Layout2 />} />
-          
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+                    <Route index element={<LayoutPage />} />
+                    <Route path="overview" element={<OverviewPage />} />
+
+                    <Route path="patients" element={<PatientsPage />} />
+                    <Route path="appointments" element={<AppointmentsPage />} />
+                    <Route path="payments" element={<PaymentsPage />} />
+
+                    <Route path="test" element={<TestPage />} />
+                
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
